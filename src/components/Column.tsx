38be468edf0ac362d533668c6732102045a1c6df
jambis <imjaamees@gmail.com>
2020-05-20 22:50:23 -0500

    Basic scaffolding of components

---
+++ src/components/Column.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Column = (props: Props) => {
+  return <div>This will be a column container</div>;
+};
+
+export default Column;
---
+++ src/components/ColumnItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const ColumnItem = (props: Props) => {
+  return <div>Item in a column</div>;
+};
+
+export default ColumnItem;
