8918f777a7ae0ea16eaec37dfb7e88556ee42189
jambis <imjaamees@gmail.com>
2020-05-22 13:15:16 -0500

    Added column number to be passed to the card

--- src/components/ColumnItem.tsx
+++ src/components/ColumnItem.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ItemsState } from "../store/ItemsData/types";
+
 interface IProps {
   data: ItemsState;
+  column: number;
 }
 
-const ColumnItem = ({ data }: IProps) => {
+const ColumnItem = ({ data, column }: IProps) => {
   return <div>{data.text}</div>;
 };
 
