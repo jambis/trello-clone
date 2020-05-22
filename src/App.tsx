a11be3dfb249054e26fa543aa4a7cf06bde04145
jambis <imjaamees@gmail.com>
2020-05-21 20:51:02 -0500

    Pulling state from redux for columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+import { useSelector } from "react-redux";
+import { ColumnsState } from "./store/ColumnsData/types";
+import { ItemsState } from "./store/ItemsData/types";
 import "./App.css";
 
+interface RootState {
+  columns: ColumnsState[];
+  items: ItemsState[];
+}
+
 function App() {
-  return <div className="App">Trello Clone</div>;
+  const selectColumns = (state: RootState) => state.columns;
+  const columns = useSelector(selectColumns);
+
+  console.log(columns);
+  return (
+    <div className="App">
+      Trello Clone
+      {columns.map((column) => {
+        return <div>{column.title}</div>;
+      })}
+      <button>Add New Column</button>
+    </div>
+  );
 }
 
 export default App;
