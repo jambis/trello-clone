db7053b8e32ef4f27ee3933a80ea4b817a6e0899
jambis <imjaamees@gmail.com>
2020-05-23 20:24:05 -0500

    Added switchColumns action

--- src/store/ColumnsData/actions.ts
+++ src/store/ColumnsData/actions.ts
@@ -1,8 +1,20 @@
-import { ADD_COLUMN } from "./types";
+import { ADD_COLUMN, SWITCH_COLUMNS } from "./types";
 
 export function addColumn(title: string) {
   return {
     type: ADD_COLUMN,
     payload: title,
+  };
+}
+
+export function switchColumns(
+  dragId: number,
+  dropId: number,
+  dragOrder: number,
+  dropOrder: number
+) {
+  return {
+    type: SWITCH_COLUMNS,
+    payload: { dragId, dropId, dragOrder, dropOrder },
   };
 }
