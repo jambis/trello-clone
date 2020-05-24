ca3b00780a680fb8c47f0433177691acca9b8441
jambis <imjaamees@gmail.com>
2020-05-23 20:26:03 -0500

    Added types for switch columns

--- src/store/ColumnsData/types.ts
+++ src/store/ColumnsData/types.ts
@@ -7,10 +7,21 @@ export interface ColumnsState {
 
 // Describing the different ACTION NAMES available
 export const ADD_COLUMN = "ADD_COLUMN";
+export const SWITCH_COLUMNS = "SWITCH_COLUMNS";
 
 interface AddColumnAction {
   type: typeof ADD_COLUMN;
   payload: string;
 }
 
-export type ColumnActionTypes = AddColumnAction;
+interface SwitchColumnsAction {
+  type: typeof SWITCH_COLUMNS;
+  payload: {
+    dragId: number;
+    dropId: number;
+    dragOrder: number;
+    dropOrder: number;
+  };
+}
+
+export type ColumnActionTypes = AddColumnAction | SwitchColumnsAction;
